c04ed8adc7f3197d1d7fe48e7f5daf16e1d2a6ea
Cameron Podd <podd@usc.edu>
2019-07-03 09:59:02 -0700

    Add misc files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tdhbackend
-Backend for the. dining. hall. app at USC
+Backend for the. dining. hall. app at USC.
+See admin for .env file configuration.
+
+# With Yarn:
+
+	To start: "yarn"
+	Run Program: "yarn start"
+	Run Tests: "yarn test"
+	Debug with Live Reloading: "yarn watch"
+
+# With npm
+	To start: "npm install"
+	Run Program: "npm start"
+	Run Tests: "npm test"
+	Debug with live reloading: "npm watch"
--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 require('dotenv').config();
-const app = require('express');
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+const cors = require('cors');
+
+const errorHandler = require('./apis/error');
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  const err = new Error('Route not found :(');
+  err.status = 404;
+  err.data = req.route;
+  next(err);
+});
+
+app.use(errorHandler);
+app.listen(port, () => console.log(`Listening on port ${port}!`));
